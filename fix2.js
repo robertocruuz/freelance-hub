ee8df146dd7a46287eb9cbc31cb3bab0e74a4287
Roberto <cruzrondan@gmail.com>
2026-03-24 18:21:59 -0300

    limpeza de arquivos de teste

--- fix2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('src/pages/ProjectsPage.tsx', 'utf8').split('\n');
-if (lines[919] && lines[919].includes('{items.map(item => {')) {
-  lines.splice(919, 1184 - 919 + 1);
-  fs.writeFileSync('src/pages/ProjectsPage.tsx', lines.join('\n'));
-  console.log('Fixed syntax error!');
-} else {
-  console.log('Line 919 was not {items.map(item => { :', lines[919]);
-}
